455e94bc907ed1cb5a4b2dcfd47286a129e6d593
Will Soto <will.soto9@gmail.com>
2017-07-03 17:41:12 -0400

    chore(eslint): allow double quotes to avoid escape
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,13 @@ module.exports = {
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true
+      }
+    ],
     semi: ['error', 'always'],
     'import/order': [
       2,
